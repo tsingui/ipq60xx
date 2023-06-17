b6183edfeb427def4bb137082efa3214155f96e8
JiaY-shi <shi05275@163.com>
2023-06-17 17:03:04 +0800

    QualcommAX: ipq60xx: add support for GL.iNet gl-ax1800 and gl-axt1800

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -10,6 +10,10 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
+	glinet,gl-axt1800|\
+	glinet,gl-ax1800)
+		nand_do_upgrade "$1"
+		;;
 	*)
 		default_do_upgrade "$1"
 		;;
