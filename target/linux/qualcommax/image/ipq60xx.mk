0b20f464d915ee8deb0bc3cab1d3b09b424a41cc
JiaY-shi <shi05275@163.com>
2023-09-21 18:37:36 +0800

    QualcommAX: ipq60xx: add support for JD Cloud AX1800 Pro

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -11,6 +11,12 @@ define Device/UbiFit
 	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
+define Device/EmmcImage
+	IMAGES += factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+endef
+
 define Device/glinet_gl-ax1800
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
@@ -38,3 +44,20 @@ define Device/glinet_gl-axt1800
 		kmod-fs-exfat kmod-hwmon-pwmfan block-mount kmod-usb-storage kmod-usb2 fdisk
 endef
 TARGET_DEVICES += glinet_gl-axt1800
+
+
+define Device/jdc_ax1800-pro
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JD Cloud
+	DEVICE_MODEL := JDC AX1800 Pro
+	DEVICE_DTS_CONFIG := config@cp03-c2
+	DEVICE_DTS := ipq6018-jdc-ax1800-pro
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-jdc_ax1800-pro kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
+	BLOCKSIZE := 64k
+	IMAGES += kernel.bin rootfs.bin
+	IMAGE/kernel.bin := append-kernel
+	IMAGE/rootfs.bin := append-rootfs | pad-rootfs | pad-to $$(BLOCKSIZE)
+endef
+TARGET_DEVICES += jdc_ax1800-pro
