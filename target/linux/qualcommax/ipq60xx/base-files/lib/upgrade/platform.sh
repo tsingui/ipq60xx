b3343836d38971d6db968ea69980807036257a6c
pexcn <pexcn97@gmail.com>
2023-10-29 18:14:28 +0800

    qualcommax: add Xiaomi AX1800 support
    references:
    - https://github.com/coolsnowwolf/openwrt-gl-ax1800/commit/0fc6174
    - https://forum.openwrt.org/t/adding-openwrt-support-for-netgear-rax120-nighthawk-ax12/75592/63

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -17,7 +17,8 @@ platform_pre_upgrade() {
 platform_do_upgrade() {
 	case "$(board_name)" in
 	glinet,gl-axt1800|\
-	glinet,gl-ax1800)
+	glinet,gl-ax1800|\
+	xiaomi,ax1800)
 		nand_do_upgrade "$1"
 		;;
 	jdc,ax1800-pro)
